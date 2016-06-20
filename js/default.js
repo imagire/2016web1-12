c11db6438f774769c1e49e6d401c97859d16d45f
Takashi Imagire <imagire@gmail.com>
2016-06-21 00:15:44 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
-var li = document.createElement('li');
-li.textContent = 'アイテム';
-document.getElementById('list').appendChild(li);
+for(var i = 0; i < 10; i++){
+  var li = document.createElement('li');
+  li.textContent = 'i;
+  document.getElementById('list').appendChild(li);
+}
