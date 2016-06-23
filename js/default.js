fc617256c8178c6c507d93bd52afa765b8c2e575
Takashi Imagire <imagire@gmail.com>
2016-06-23 17:20:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -25,3 +25,18 @@ for(var i = 1; i < 10; i++){
   document.getElementById('price').appendChild(li);
 }
 
+
+for(var i = 1; i < 100; i++){
+  var li = document.createElement('li');
+  if( i % 15 == 0){
+    li.textContent = 'FizzBuzz!';
+  }else if(i % 3 == 0){
+    li.textContent = 'Fizz';
+  }else if(i % 5 == 0){
+    li.textContent = 'Buzz';
+  }else {
+    li.textContent = i;
+  }
+  document.getElementById('list').appendChild(li);
+}
+
