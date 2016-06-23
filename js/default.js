9a53e0d4e6d37fb4b81dfa70a167d78ffd1bc189
Takashi Imagire <imagire@gmail.com>
2016-06-23 19:51:09 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -26,7 +26,7 @@ for(var i = 1; i < 10; i++){
 }
 
 var constellatio = ['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];
-for(var i = 0; i < 12; i++){
+for(var i = 0; i < constellatio.length; i++){
   var li = document.createElement('li');
   li.textContent = constellatio[i];
   document.getElementById('array').appendChild(li);
