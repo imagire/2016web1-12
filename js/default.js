ac442adbf0a7d37b10963eddd1069243d36e8ad2
Takashi Imagire <imagire@gmail.com>
2016-06-23 16:07:43 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
-for(var i = 0; i < 10; i++){
+for(var i = 1; i < 10; i++){
   var li = document.createElement('li');
-  li.textContent = i;
+  li.textContent = i + '回目';
   document.getElementById('list').appendChild(li);
 }
